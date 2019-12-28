39fec9ca2a6fcf1280a55904244722099236f33d
Lana Bozanic <56446380+lanabozanic@users.noreply.github.com>
2019-12-27 23:23:20 -0500

    Update and rename Bernstein-VaziraniAlgorithm.py-2.html to Bernstein-VaziraniAlgorithm.py

--- Bernstein-VaziraniAlgorithm.py
+++ Bernstein-VaziraniAlgorithm.py
@@ -1,80 +1,45 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#Import Libraries
 from qiskit import *
 from qiskit.visualization import plot_histogram
 
-
-# In[2]:
-
-
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[3]:
-
-
+#Secret bitstring we're trying to find
 secret = '10001101'
-
-
-# In[4]:
-
 
 circuit = QuantumCircuit(len(secret)+1, len(secret))
 
+#put a hadamard on every gate (except the last one)
 circuit.h(range(len(secret)))
-          
-circuit.x(len(secret))
-          
+  
+#apply a Pauil X (quantum NOT) and a hadamard on the last qubit (for phase kickback)
+circuit.x(len(secret))          
 circuit.h(len(secret))
 
 circuit.barrier()
 
+#for ever 1 in the bitstring, apply a CNOT
 for i, x in enumerate(reversed(secret)):
     if x == '1':
         circuit.cx(i, len(secret))
 
-
+#for visualization purposes
 circuit.barrier()
 
+#re-hadamard all the gates. Every superposition |- > becomes 1 and everyone |+ > becomes 0
 circuit.h(range(len(secret)))
 
+#for visualization purposes
 circuit.barrier()
+
+#measure and store results
 circuit.measure(range(len(secret)), range(len(secret)))
 
-
-# In[5]:
-
-
+#draw the circuit
 circuit.draw(output='mpl')
 
-
-# In[6]:
-
-
+#run the code on a simulator
 sim = Aer.get_backend('qasm_simulator')
 results = execute(circuit, backend=sim, shots=1).result()
 counts = results.get_counts()
 print(counts)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
